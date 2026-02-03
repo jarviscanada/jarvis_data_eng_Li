110a3bedd1397c612169368fadcffcedff3cbb8e
lijason982 <91034907+lijason982@users.noreply.github.com>
2026-02-03 13:24:27 -0500

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,46 +1,4 @@
 -- =========================
--- Table Setup (DDL)
--- =========================
-
-CREATE SCHEMA IF NOT EXISTS cd;
-
-CREATE TABLE cd.members (
-    memid INTEGER NOT NULL,
-    surname VARCHAR(200) NOT NULL,
-    firstname VARCHAR(200) NOT NULL,
-    address VARCHAR(300) NOT NULL,
-    zipcode INTEGER NOT NULL,
-    telephone VARCHAR(20) NOT NULL,
-    recommendedby INTEGER,
-    joindate TIMESTAMP NOT NULL,
-    CONSTRAINT members_pk PRIMARY KEY (memid),
-    CONSTRAINT members_recommendedby_fk FOREIGN KEY (recommendedby)
-        REFERENCES cd.members(memid)
-        ON DELETE SET NULL
-);
-
-CREATE TABLE cd.facilities (
-    facid INTEGER NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    membercost NUMERIC NOT NULL,
-    guestcost NUMERIC NOT NULL,
-    initialoutlay NUMERIC NOT NULL,
-    monthlymaintenance NUMERIC NOT NULL,
-    CONSTRAINT facilities_pk PRIMARY KEY (facid)
-);
-
-CREATE TABLE cd.bookings (
-    bookid INT NOT NULL,
-    facid INT NOT NULL,
-    memid INT NOT NULL,
-    starttime TIMESTAMP NOT NULL,
-    slots INT NOT NULL,
-    CONSTRAINT bookings_pk PRIMARY KEY (bookid),
-    CONSTRAINT bookings_facid_fk FOREIGN KEY (facid) REFERENCES cd.facilities(facid),
-    CONSTRAINT bookings_memid_fk FOREIGN KEY (memid) REFERENCES cd.members(memid)
-);
-
--- =========================
 -- SQL Queries / Exercises
 -- =========================
 
@@ -120,3 +78,4 @@ LEFT JOIN cd.members r ON r.memid = m.recommendedby
 ORDER BY 2,1;
 
 -- Continue adding remaining queries in same format...
+
