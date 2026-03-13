6cdfe393a4c7416f20cf46b8adbd857ff0677388
Jason Li <lijason982@gmail.com>
2026-03-13 15:29:35 +0000

    stream api changes

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepLambdaImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepLambdaImp.java
@@ -1,22 +1,21 @@
 package ca.jrvs.apps.grep;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.io.*;
+import java.util.*;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 import org.apache.log4j.BasicConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class JavaGrepLambdaImp extends JavaGrepImp {
+public class JavaGrepLambdaImp implements JavaGrepLambda {
 
   private static final Logger logger = LoggerFactory.getLogger(JavaGrepLambdaImp.class);
+
+  private String regex;
+  private String rootPath;
+  private String outFile;
+  private Pattern pattern;
 
   public static void main(String[] args) {
     if (args.length != 3) {
@@ -38,45 +37,89 @@ public class JavaGrepLambdaImp extends JavaGrepImp {
   }
 
   @Override
+  public String getRootPath() { return rootPath; }
+
+  @Override
+  public void setRootPath(String rootPath) { this.rootPath = rootPath; }
+
+  @Override
+  public String getRegex() { return regex; }
+
+  @Override
+  public void setRegex(String regex) {
+    this.regex = regex;
+    this.pattern = Pattern.compile(regex);
+  }
+
+  @Override
+  public String getOutFile() { return outFile; }
+
+  @Override
+  public void setOutFile(String outFile) { this.outFile = outFile; }
+
+  @Override
   public void process() throws IOException {
-    List<String> matchedLines = listFiles(getRootPath()).stream().
-        flatMap(file -> readLines(file).stream()).filter(this::containsPattern).
-        collect(Collectors.toList());
+    Stream<String> matchedLines = listFiles(getRootPath())
+        .flatMap(this::readLines)
+        .filter(this::containsPattern);
     writeToFile(matchedLines);
   }
 
   @Override
-  public List<File> listFiles(String rootDir) {
-    File root = new File(rootDir);
-    if (root.isFile()) {
-      return Collections.singletonList(root);
+  public void writeToFile(Stream<String> lines) throws IOException {
+    if (lines == null) throw new IllegalArgumentException("No lines");
+    try (BufferedWriter bw = new BufferedWriter(new FileWriter(getOutFile()))) {
+      lines.forEach(line -> {
+        try {
+          bw.write(line);
+          bw.newLine();
+        } catch (IOException ex) {
+          logger.error("Cannot write to file", ex);
+        }
+      });
     }
-    File[] leaves = root.listFiles();
-    if (leaves == null) {
-      return new ArrayList<>();
-    }
-    return Arrays.stream(leaves).
-        flatMap(file -> {
-          if (file.isDirectory()){
-          return listFiles(file.getAbsolutePath()).stream();
-          } else {
-            return Stream.of(file);
-          }
-    }).collect(Collectors.toList());
   }
 
   @Override
-  public List<String> readLines(File inputFile) {
-    if (inputFile == null || !inputFile.isFile()) {
-      throw new IllegalArgumentException("Invalid file");
+  public Stream<File> listFiles(String rootDir) {
+    File root = new File(rootDir);
+    if (root.isFile()){
+      return Stream.of(root);
     }
-    try (BufferedReader br = new BufferedReader(new FileReader(inputFile))){
-      return br.lines().collect(Collectors.toList());
-    } catch (IOException ex) {
-      logger.error("Error reading file", ex);
+    File[] leaves = root.listFiles();
+    if (leaves == null){
+      return Stream.empty();
     }
-    return new ArrayList<>();
+    return Arrays.stream(leaves)
+        .flatMap(file -> {
+          if (file.isDirectory()) {
+            return listFiles(file.getAbsolutePath());
+          } else {
+            return Stream.of(file);
+          }
+        });
   }
 
-}
+  @Override
+  public Stream<String> readLines(File inputFile) {
+    if (inputFile == null || !inputFile.isFile())
+      throw new IllegalArgumentException("Invalid file");
+    try {
+      BufferedReader br = new BufferedReader(new FileReader(inputFile));
+      return br.lines().onClose(() -> {
+        try {
+          br.close();
+        } catch (IOException ex) {
+          logger.error("Error closing reader", ex);
+        }
+      });
+    } catch (IOException ex) {
+      logger.error("Cannot read file", ex);
+      return Stream.empty();
+    }
+  }
 
+  public boolean containsPattern(String line) {
+    return pattern.matcher(line).find();
+  }
+}
